920c51a1894cfe15dc3391527f44f4f4d63bd0fd
Caesaraya <caesaraya0707@gmail.com>
2025-05-05 09:25:30 +0700

    hai

--- app/src/main/java/com/example/tugaspakaji/api/ApiService.java
+++ app/src/main/java/com/example/tugaspakaji/api/ApiService.java
@@ -7,6 +7,6 @@ import retrofit2.http.GET;
 import retrofit2.http.Query;
 
 public interface ApiService {
-    @GET("https://www.thesportsdb.com/api/v1/json/3/search_all_teams.php?l=English%20Premier%20League")
+    @GET("search_all_teams.php?l=English%20Premier%20League")
     Call<TeamResponse> getAllTeams();
 }
