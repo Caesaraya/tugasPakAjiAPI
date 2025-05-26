f1b7a48416bb97a13a736188b523dfeff44d22e3
Caesaraya <caesaraya0707@gmail.com>
2025-05-26 10:35:34 +0700

    feat [Caesaraya] api2

--- app/src/main/java/com/example/tugaspakaji/api/ApiService.java
+++ app/src/main/java/com/example/tugaspakaji/api/ApiService.java
@@ -9,4 +9,7 @@ import retrofit2.http.Query;
 public interface ApiService {
     @GET("search_all_teams.php?l=English%20Premier%20League")
     Call<TeamResponse> getAllTeams();
+
+    @GET("search_all_teams.php?s=Soccer&c=Spain")
+    Call<TeamResponse> getAllTeams2();
 }
